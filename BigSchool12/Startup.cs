34f5a50cb1b73602e8409592198546abd10156b7
HaoNguyen3012001 <106661040+HaoNguyen3012001@users.noreply.github.com>
2022-06-21 23:50:01 +0700

    Add project files.

---
+++ BigSchool12/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool12.Startup))]
+namespace BigSchool12
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
